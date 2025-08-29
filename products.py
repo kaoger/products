1d07d4eb7583caa68d26b6efd2b05b520dcea2d7
kaoger <rend579@gmail.com>
2025-08-29 09:53:24 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,8 @@
+products =[]
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格: ')
+    products.append(['商品名稱: '+name,'商品價格: ' + price])
+print(products)
